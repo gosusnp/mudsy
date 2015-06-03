d24581374b847b63511e8821470b28f3eba2bbb7
snp <snp@skee.fr>
2015-06-03 08:26:34 +0200

    Improve analytics.
    Only fetch the script once.

--- client/assets/js/mudsy.js
+++ client/assets/js/mudsy.js
@@ -108,19 +108,8 @@
                 });
             }
 
-            var analytics = document.getElementById('analytics');
-            var script = document.createElement('script');
-            script.textContent =
-                "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"+
-                "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"+
-                "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"+
-                "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"+
-                "ga('create', 'UA-41357909-3', 'auto');"+
-                "ga('send', 'pageview');";
-            if (analytics.hasChildNodes()) {
-                analytics.removeChild(analytics.childNodes[0]);
-            }
-            analytics.appendChild(script);
+            window.ga('create', 'UA-41357909-3', 'auto');
+            window.ga('send', 'pageview');
         }
     ]);
 
@@ -131,6 +120,12 @@
         .state('mudsy', {
             url: '/mudsy',
             template: '<div ui-view></div>',
+            controller: function() {
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            },
             abstract: true
         })
         .state('mudsy.artists', {
