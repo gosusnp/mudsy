0e1cc05c019988f53cbd66416c5eebbd17c1dbbc
snp <snp@skee.fr>
2015-06-03 08:10:28 +0200

    Analytics quick fix.

--- client/assets/js/mudsy.js
+++ client/assets/js/mudsy.js
@@ -107,6 +107,20 @@
                     $scope.similar = null;
                 });
             }
+
+            var analytics = document.getElementById('analytics');
+            var script = document.createElement('script');
+            script.textContent =
+                "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"+
+                "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"+
+                "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"+
+                "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"+
+                "ga('create', 'UA-41357909-3', 'auto');"+
+                "ga('send', 'pageview');";
+            if (analytics.hasChildNodes()) {
+                analytics.removeChild(analytics.childNodes[0]);
+            }
+            analytics.appendChild(script);
         }
     ]);
 
